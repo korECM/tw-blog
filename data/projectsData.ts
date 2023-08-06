070ffe5cee18d9dd89948cffc664bcf88b807d0c
Jongwon <youncookiexpire@kakao.com>
2023-08-07 01:03:28 +0900

    fix type error

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,3 +1,4 @@
-const projectsData = []
+type ProjectDataType = { description: string; href: string; title: string; imgSrc: string }
+const projectsData: ProjectDataType[] = []
 
 export default projectsData
