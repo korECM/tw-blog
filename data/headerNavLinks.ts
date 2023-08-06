f9fa5c5b9664e1d0b95b6859cc40627b672c9c53
Jongwon <youncookiexpire@kakao.com>
2023-08-06 16:34:31 +0900

    초기설정

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,13 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  {
+    href: 'https://korecm.oopy.io/?utm_source=blog&utm_medium=navbar&utm_campaign=just_link',
+    title: 'Resume',
+  },
+  // { href: '/projects', title: 'Projects' },
+  // { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
---
+++ data/siteMetadata.js
@@ -0,0 +1,91 @@
+/** @type {import("pliny/config").PlinyConfig } */
+const siteMetadata = {
+  title: '쿠키의 개발 블로그',
+  author: 'ECM',
+  headerTitle: '쿠키의 개발 블로그',
+  description: '',
+  language: 'ko-kr',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://www.korecmblog.com',
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteLogo: '/static/images/logo.png',
+  socialBanner: '/static/images/twitter-card.png',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'eatingcookieman@gmail.com',
+  github: 'https://github.com/korECM',
+  // twitter: 'https://twitter.com/Twitter',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/jongwon-youn-4389a4197',
+  locale: 'ko-kr',
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    // umamiAnalytics: {
+    //   // We use an env variable for this site to avoid other users cloning our analytics ID
+    //   umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-G8RQW6862C', // e.g. G-XXXXXXX
+    },
+  },
+  // newsletter: {
+  //   // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
+  //   // Please add your .env file and modify it according to your selection
+  //   provider: 'buttondown',
+  // },
+  comments: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // Select a provider and use the environment variables associated to it
+    // https://vercel.com/docs/environment-variables
+    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    giscusConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://giscus.app/
+      repo: 'korECM/blog',
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk0MDAyMTQ3NTY=',
+      category: 'Comments',
+      categoryId: 'DIC_kwDOF9rK5M4CYYQF',
+      mapping: 'pathname', // supported options: pathname, url, title
+      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      metadata: '0',
+      // theme example: light, dark, dark_dimmed, dark_high_contrast
+      // transparent_dark, preferred_color_scheme, custom
+      theme: 'preferred_color_scheme',
+      // theme when dark mode
+      darkTheme: 'transparent_dark',
+      // If the theme option above is set to 'custom`
+      // please provide a link below to your custom theme css file.
+      // example: https://giscus.app/themes/custom_example.css
+      themeURL: '',
+      // This corresponds to the `data-lang="en"` in giscus's configurations
+      lang: 'ko',
+    },
+  },
+  search: {
+    // provider: 'kbar', // kbar or algolia
+    // kbarConfig: {
+    //   searchDocumentsPath: 'search.json', // path to load documents to search
+    // },
+    provider: 'algolia',
+    algoliaConfig: {
+      // The application ID provided by Algolia
+      appId: 'K8KSGENH0V',
+      // Public API key: it is safe to commit it
+      apiKey: 'f8e68fa2b3937c08aad6c57bb5a14437',
+      indexName: 'blog',
+    },
+  },
+}
+
+module.exports = siteMetadata
