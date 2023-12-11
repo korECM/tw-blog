4eecad68b12af887b2c923363bb38e271cf2aa7e
Jongwon <youncookiexpire@kakao.com>
2023-12-11 21:19:03 +0900

    Resume 링크 수정

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   {
-    href: 'https://korecm.oopy.io/?utm_source=blog&utm_medium=navbar&utm_campaign=just_link',
+    href: 'http://resume.korecmblog.com/?utm_source=blog&utm_medium=navbar&utm_campaign=just_link',
     title: 'Resume',
   },
   // { href: '/projects', title: 'Projects' },
